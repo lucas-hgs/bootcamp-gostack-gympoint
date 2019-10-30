15188c600b368cd7a1b6d102dcc7610c3b5fcd3d
Lucas Henrique Goncalves da Silveira <lucas.hgs03@gmail.com>
2019-10-30 20:05:05 -0300

    Created Auth Middleware and User Update

--- src/routes.js
+++ src/routes.js
@@ -4,10 +4,15 @@ import UserController from './app/controllers/UserController';
 import StudentController from './app/controllers/StudentController';
 import SessionController from './app/controllers/SessionController';
 
+import authMiddleware from './app/middlewares/auth';
+
 const routes = new Router();
 
 routes.post('/users', UserController.store);
-routes.post('/students', StudentController.store);
 routes.post('/sessions', SessionController.store);
+
+routes.use(authMiddleware);
+
+routes.post('/students', StudentController.store);
 
 export default routes;
