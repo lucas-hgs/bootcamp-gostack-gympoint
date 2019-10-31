b60635c3b72ff35de86d3cec131bf6cf0807d1b1
Lucas Henrique Goncalves da Silveira <lucas.hgs03@gmail.com>
2019-10-30 23:31:48 -0300

    Finished the Student Controller

--- src/app/controllers/StudentController.js
+++ src/app/controllers/StudentController.js
@@ -1,8 +1,32 @@
+import * as Yup from 'yup';
+
 import Student from '../models/Student';
 
 class StudentController {
   async store(req, res) {
-    const studentExists = Student.findOne({ where: { email: req.body.email } });
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      email: Yup.string()
+        .email()
+        .required(),
+      age: Yup.number()
+        .moreThan(0)
+        .required(),
+      weight: Yup.number()
+        .moreThan(0)
+        .required(),
+      height: Yup.number()
+        .moreThan(0)
+        .required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' });
+    }
+
+    const studentExists = await Student.findOne({
+      where: { email: req.body.email },
+    });
 
     if (studentExists) {
       return res.status(400).json({ error: 'Student already exists' });
@@ -11,6 +35,45 @@ class StudentController {
     const { id, name, email, age, weight, height } = await Student.create(
       req.body
     );
+
+    return res.json({
+      id,
+      name,
+      email,
+      age,
+      weight,
+      height,
+    });
+  }
+
+  async update(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string(),
+      email: Yup.string().email(),
+      age: Yup.number().moreThan(0),
+      weight: Yup.number().moreThan(0),
+      height: Yup.number().moreThan(0),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' });
+    }
+
+    const { email } = req.body;
+
+    const student = await Student.findByPk(req.params.id);
+
+    if (email !== student.email) {
+      const studentExists = await Student.findOne({
+        where: { email },
+      });
+
+      if (studentExists) {
+        return res.status(400).json({ error: 'Student already exists' });
+      }
+    }
+
+    const { id, name, age, weight, height } = await student.update(req.body);
 
     return res.json({
       id,
